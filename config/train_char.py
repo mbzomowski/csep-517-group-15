c15f549dc8f1b12bea6359c9e99d7ccd400b4984
Mark Bzomowski <mbzomowski@gmail.com>
2025-05-22 05:12:21 +0000

    3k iters on a larger model

--- config/train_char.py
+++ config/train_char.py
@@ -19,15 +19,15 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.1
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 3e-4 # with baby networks can afford to go a bit higher
 max_iters = 5000
 lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+min_lr = 3e-5 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
